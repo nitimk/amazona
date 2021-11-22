92ec6f9b6b2e05051837c20414f2ee6c8add5611
Niti <nitimk@gmail.com>
2021-11-22 15:33:45 -0500

    redux implementation

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -60,7 +60,7 @@ export default function ProductScreen() {
                     {product.countInStock > 0 ? (
                       <span className="success">In Stock</span>
                     ) : (
-                      <span className="error">Unavailable</span>
+                      <span className="danger">Unavailable</span>
                     )}
                   </div>
                 </div>
