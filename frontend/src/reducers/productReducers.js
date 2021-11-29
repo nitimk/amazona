3a09b272ed374e2365c03edfa81dd6e427dbd35f
Niti <nitimk@gmail.com>
2021-11-29 10:31:59 -0500

    data fatch

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -2,6 +2,8 @@ const {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
 } = require("../constants/productConstants");
 
 export const productListReducer = (
@@ -13,6 +15,22 @@ export const productListReducer = (
       return { loading: true };
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload };
+    case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productDetailsReducer = (
+  state = { product: {}, loading: true },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true };
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload };
     case PRODUCT_LIST_FAIL:
       return { loading: false, error: action.payload };
     default:
