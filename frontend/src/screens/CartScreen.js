2b1407523a92fa3dc21497ccaa951639800527e3
Niti <nitimk@gmail.com>
2021-12-08 15:52:42 -0500

    cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import {   useParams, useLocation } from "react-router-dom";
+import { addToCart } from "../actions/cartActions";
 
 export default function CartScreen() {
   const { productId } = useParams();
   const location = useLocation();
   const qty = location.pathname ? Number(location.search.split("=")[1]) : 1;
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId,qty]);
+
   return (
     <div>
       <h1> Cart Screen </h1>
