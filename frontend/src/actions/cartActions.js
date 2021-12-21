c1e55c5345349725144770d292f09bc5a9a03aef
Niti <nitimk@gmail.com>
2021-12-21 09:44:36 -0500

    Shipping form

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,9 @@
 import Axios from "axios";
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+} from "../constants/cartConstants";
 
 export const addToCart = (productId, qty) => async (dispatch, getstate) => {
   const { data } = await Axios.get(`/api/products/${productId}`);
@@ -14,10 +18,15 @@ export const addToCart = (productId, qty) => async (dispatch, getstate) => {
       qty,
     },
   });
-  localStorage.setItem('cartItems', JSON.stringify(getstate().cart.cartItems));
+  localStorage.setItem("cartItems", JSON.stringify(getstate().cart.cartItems));
 };
 
 export const removeFromCart = (productId) => (dispatch, getstate) => {
-    dispatch({ type: CART_REMOVE_ITEM, payload: productId });
-    localStorage.setItem('cartItem', JSON.stringify(getstate().cart.cartItems));
+  dispatch({ type: CART_REMOVE_ITEM, payload: productId });
+  localStorage.setItem("cartItem", JSON.stringify(getstate().cart.cartItems));
+};
+
+export const saveShippingAddress = (data) => (dispatch) => {
+  dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: data });
+  localStorage.setItem('shippingAddress', JSON.stringify(data));
 };
--- frontend/src/screens/ShippingAddressScreen.js
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -1,10 +1,102 @@
-import React from 'react'
-import CheckoutSteps from '../components/CheckoutSteps'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { saveShippingAddress } from "../actions/cartActions";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 export default function ShippingAddressScreen() {
-    return (
+  const userSignin = useSelector((state) => state.userSignin);
+  const navigate = useNavigate();
+  const { userInfo } = userSignin;
+  if (!userInfo) {
+     navigate("/signin");
+  }
+const [fullName, setFullName] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [country, setCountry] = useState("");
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(saveShippingAddress(fullName, address, city, postalCode, country));
+    // TODO: dispatch save shipping address action
+  };
+  //const navigate = useNavigate();
+  navigate("/payment");
+  return (
+    <div>
+      <CheckoutSteps step1 step2></CheckoutSteps>
+      <form className="form" onSubmit={submitHandler}>
         <div>
-            <CheckoutSteps step1 step2></CheckoutSteps>
+          <h1>Shippping Address</h1>
         </div>
-    );
+        <div>
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            type="text"
+            id="fullName"
+            placeholder="Enter full name"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+            required
+          ></input>
+        </div>
+
+        <div>
+          <label htmlFor="address">Address</label>
+          <input
+            type="text"
+            id="address"
+            placeholder="Enter full address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            required
+          ></input>
+        </div>
+
+        <div>
+          <label htmlFor="city">City</label>
+          <input
+            type="text"
+            id="city"
+            placeholder="Enter city"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            required
+          ></input>
+        </div>
+
+        <div>
+          <label htmlFor="postalCode">Postal Code</label>
+          <input
+            type="text"
+            id="postalCode"
+            placeholder="Enter postal code"
+            value={postalCode}
+            onChange={(e) => setPostalCode(e.target.value)}
+            required
+          ></input>
+        </div>
+
+        <div>
+          <label htmlFor="country">Country</label>
+          <input
+            type="text"
+            id="country"
+            placeholder="Enter country"
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+            required
+          ></input>
+        </div>
+        <div>
+          <label />
+          <button className="primary" type="submit">
+            Continue
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
